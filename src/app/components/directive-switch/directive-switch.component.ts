5ddd2f03e0c189c5ef1d7b721dad239f195bc81f
matheusmattana <matheusmattana@hotmail.com>
2022-02-22 04:24:07 -0300

    feat(component): Adding switch default

--- src/app/components/directive-switch/directive-switch.component.ts
+++ src/app/components/directive-switch/directive-switch.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./directive-switch.component.scss'],
 })
 export class DirectiveSwitchComponent {
-  actualAnimal = 'dog';
+  actualAnimal = '';
 }
