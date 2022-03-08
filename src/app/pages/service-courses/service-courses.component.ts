14a4e2aafe947f01eb182f5363d5d7196ec7ddca
Matheus Mattana <matheusmattana@hotmail.com>
2022-03-08 10:39:21 -0300

    feat(page): Practicing the use of angular services

--- src/app/pages/service-courses/service-courses.component.ts
+++ src/app/pages/service-courses/service-courses.component.ts
@@ -1,15 +1,17 @@
+import { CoursesService } from './../../services/courses/courses.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-service-courses',
   templateUrl: './service-courses.component.html',
-  styleUrls: ['./service-courses.component.scss']
+  styleUrls: ['./service-courses.component.scss'],
 })
 export class ServiceCoursesComponent implements OnInit {
+  courses = [''];
 
-  constructor() { }
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit(): void {
+    this.courses = this.coursesService.getCourses();
   }
-
 }
