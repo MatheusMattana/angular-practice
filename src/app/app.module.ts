cb7df6627f9ff4eb964dabd2a25f5853ad5e93d6
Matheus Mattana <matheusmattana@hotmail.com>
2022-04-18 00:07:55 -0300

    feat: Practicing the use of locale id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesService } from './services/courses/courses.service';
 import { ServiceAddCoursesModule } from './pages/service-add-courses/service-add-courses.module';
 
+import ptBr from '@angular/common/locales/pt';
+registerLocaleData(ptBr);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, ServiceAddCoursesModule],
-  providers: [CoursesService],
+  providers: [
+    CoursesService,
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
